edb65c7b6480c6621ba57caae92a88d07651fa81
VincentWangzilu <65191551+VincentWangzilu@users.noreply.github.com>
2020-07-01 17:19:47 -0400

    Update Plot5.R

--- Plot5.R
+++ Plot5.R
@@ -1,3 +1,5 @@
+if(!file.exists("summarySCC_PM25.rds")){download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destifile <- "data.zip", method = "curl")}
+unzip("data.zip")
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 if(!file.exists("summarySCC_PM25.rds")){download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destifile <- "data.zip", method = "curl")}
